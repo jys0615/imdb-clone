f85d5885c9ba48dddca7c69a5bd5a2e25b522156
Yoonsuh "Jason" Jung <103824631+jys0615@users.noreply.github.com>
2024-07-08 01:02:25 +0900

    Delete repository directory

--- repository/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.imdbclone.repository;
-
-import com.example.imdbclone.model.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-}
