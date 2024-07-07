6c858bedcf75d7fd5e63f0be12dab774aefb86bf
Yoonsuh "Jason" Jung <103824631+jys0615@users.noreply.github.com>
2024-07-07 23:07:26 +0900

    Add files via upload

---
+++ repository/GenreRepository.java
@@ -0,0 +1,9 @@
+package com.example.imdbclone.repository;
+
+import com.example.imdbclone.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
